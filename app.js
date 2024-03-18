f731d8dbb5b766b5c2f42a452b59d8e0f4452e8e
AlexAinea <xo.ainea@gmail.com>
2024-03-18 10:06:39 +0300

    link

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 let totalExpenses = 0;
 
 function addExpense(){
-    const expenseNameInput = document.getElementById('expenseName');
-    const expenseAmountInput = document.getElementById('expenseAmount');
-    const expenseName = expenseNameInput.value;
-    const expenseAmount = parseFloat(expenseAmountInput.value);
+  const expenseNameInput = document.getElementById('expenseName');
+  const expenseAmountInput = document.getElementById('expenseAmount');
+  const expenseName = expenseNameInput.value;
+  const expenseAmount = parseFloat(expenseAmountInput.value);
 
-    if (expenseName.trim() === '' || isNaN(expenseAmount) || expenseAmount <= 0) {
-        alert('Please enter valid expense name and amount.');
-        return;
-      };
-    const expenseList = document.getElementById('expenseList');
-    const li = document.createElement('li');
-    li.textContent = `${expenseName}: $${expenseAmount}`;
-    expenseList.appendChild(li);
+  if (expenseName.trim() === '' || isNaN(expenseAmount) || expenseAmount <= 0) {
+      alert('Please enter valid expense name and amount.');
+      return;
+    };
+  const expenseList = document.getElementById('expenseList');
+  const li = document.createElement('li');
+  li.textContent = `${expenseName}: $${expenseAmount}`;
+  expenseList.appendChild(li);
 
-    totalExpenses += expenseAmount;
-    document.getElementById('totalExpenses').textContent = totalExpenses.toFixed(2);
-
-    expenseNameInput.value = '';
-    expenseAmountInput.value = '';
-  
-}
+  totalExpenses += expenseAmount;
+  document.getElementById('totalExpenses').textContent = totalExpenses.toFixed(2);  
+};
+function delet(){
+  expenseList.textContent=""
+  document.getElementById('totalExpenses').textContent = '0'
+};
